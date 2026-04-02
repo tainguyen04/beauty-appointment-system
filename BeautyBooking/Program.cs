ae74b4549737d7d705647445f4de8f0f4451ef99
tainguyen04 <huutai18102004@gmail.com>
2026-04-02 18:46:01 +0700

    tắt môi trường dev

--- BeautyBooking/Program.cs
+++ BeautyBooking/Program.cs
@@ -126,11 +126,11 @@ builder.Services.AddSwaggerGen(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 
