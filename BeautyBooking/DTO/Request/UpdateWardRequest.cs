34e50a22e2cec98ecd661aa5a440894f4dc2fb38
tainguyen04 <huutai18102004@gmail.com>
2026-04-14 18:59:39 +0700

    thêm isActive vào UpdateWardRequest

--- BeautyBooking/DTO/Request/UpdateWardRequest.cs
+++ BeautyBooking/DTO/Request/UpdateWardRequest.cs
@@ -9,5 +9,6 @@
         public string? FullNameEn { get; set; } 
         public double Latitude { get; set; }
         public double Longitude { get; set; }
+        public bool IsActive { get; set; }
     }
 }
